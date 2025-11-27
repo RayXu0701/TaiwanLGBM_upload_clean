7d280f34fc7f91069aaf1c1fd3a4ce398fb99294
RayXu0701 <ray920701@gmail.com>
2025-11-27 19:15:11 +0800

    這次的更新說明

--- TaiwanLGBM_0050/00.py
+++
@@ -1,7 +0,0 @@
-import ssl
-import certifi
-
-print("Certifi at:", certifi.where())
-ctx = ssl.create_default_context(cafile=certifi.where())
-print("SSL context created with certifi.")
-print("SUCCESS!")
